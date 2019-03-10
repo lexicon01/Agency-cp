798e8810c88cf4e77c2e732dffc3057591a5f614
unknown <ialex.roman@icloud.com>
2019-03-09 22:59:51 -0500

    added new js file.. jesus the load on this bitch

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ $(function () {
         navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
     });
 });
+
+/*=============================================
+                Progress Bars
+/*=============================================*/
+
+$(function () {
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 1000);
+        });
+        this.destroy();
+
+    }, {
+        offset: 'bottom-in-view'
+    });
+});
