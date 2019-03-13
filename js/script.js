477eba94fd80913be4ce313a73eaf716f5c6398f
unknown <ialex.roman@icloud.com>
2019-03-13 18:53:54 -0400

    added dem lines cool part too we getting that green son chea lk

--- js/script.js
+++ js/script.js
@@ -93,3 +93,21 @@ $(function () {
         }
     });
 });
+
+/*=============================================
+                Testimonials
+/*=============================================*/
+
+$(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        autoplay: false,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        margin: 15,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
